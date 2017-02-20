792d0fc2362adf3f19deeac1ee3e344a0c11d97c
panduo <448761421@qq.com>
2017-02-20 14:49:51 +0800

    2

--- libs/nainai/article.php
+++ libs/nainai/article.php
@@ -193,10 +193,10 @@ class Article{
 		
 		if(!$arcList) return array();
 		$arcInfo = $arcList[0];
-
+		$arcInfo['ori_keywords'] = $info['keywords'];
 		$keywords = Keyword::check($user_id,$arcInfo);
 		$arcInfo['keywords'] = $keywords;
-		$arcInfo['ori_keywords'] = $ori_keywords;
+		
 		$arcInfo['keywords_str'] = implode(',',$keywords);
 		
 		// if(DEVICE_TYPE != 'pc')
