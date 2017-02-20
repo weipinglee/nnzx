b754b11aac70ef8ff684f17fd389d7b4fb3fb267
weipinglee <weiping.lee@163.com>
2017-02-20 16:14:53 +0800

    220统一合并

--- application/runtime/pc/1509932dd2bf485817fa6137f8eeece8.php
+++ application/runtime/pc/1509932dd2bf485817fa6137f8eeece8.php
@@ -20,10 +20,11 @@
     <div class="content">
         <div class="logo"></div>
         <ul class="nav">
-            <li><a href='http://info.nainaiwang.com//index/index' class="on">首页</a></li>
-            <li id=""><a href='http://info.nainaiwang.com//hangye/index/type/<?php echo isset($typelist[0]['id'])?$typelist[0]['id']:"";?>'>行业</a></li>
-            <li><a href="http://info.nainaiwang.com//shuju/index/type/<?php echo isset($typelist[1]['id'])?$typelist[1]['id']:"";?>">数据</a></li>
-            <li><a href='http://info.nainaiwang.com//guandian/index/type/<?php echo isset($typelist[2]['id'])?$typelist[2]['id']:"";?>'>观点</a></li>
+            <li><a href='http://info.nainaiwang.com//index/index' <?php if($type_id == 0){?>class='on'<?php }?>>首页</a></li>
+            <li id="industry"><a href='http://info.nainaiwang.com//hangye/index/type/<?php echo isset($typelist[0]['id'])?$typelist[0]['id']:"";?>' <?php if($type_id ==$typelist[0]['id']){?>class="on"<?php }?>>行业</a></li>
+
+            <li><a href="http://info.nainaiwang.com//shuju/index/type/<?php echo isset($typelist[1]['id'])?$typelist[1]['id']:"";?>" <?php if($type_id == $typelist[1]['id']){?>class="on"<?php }?>>数据</a></li>
+            <li><a href='http://info.nainaiwang.com//guandian/index/type/<?php echo isset($typelist[2]['id'])?$typelist[2]['id']:"";?>' <?php if($type_id == $typelist[2]['id']){?>class="on"<?php }?>>观点</a></li>
         </ul>
         <form class="search">
             <input type="text" placeholder="请输入关键字查询" class="text"><input type="button" value="搜索" class="button">
@@ -38,20 +39,22 @@
             });
         })
     </script>
+    
+    <div class="in_nav" id="in_nav">
+        <ul>
+            <img src="/views/pc/images/nav_top.png" class="in_nav_top">
 
+            <?php if(!empty($cates)) foreach($cates as $key => $item){?>
+            <li>
+                <a class="title_a" href="http://info.nainaiwang.com//hangye/index/type/3/id/<?php echo isset($item['id'])?$item['id']:"";?>"><?php echo isset($item['name'])?$item['name']:"";?>
+                    <div class="c-tip-arrow" style="left: 38px;"><em></em></div>
+                </a>
+            </li>
+            <?php }?>
+            <!-- <li class="industry1"><a href>建材行业</a></li> -->
+        </ul>
+    </div>
 
-
-	<div class="in_nav" id="in_nav">
-		<ul>
-			<img src="/views/pc/images/nav_top.png" class="in_nav_top">
-			<li class="industry1"><a href>建材行业</a></li>
-			<li class="industry2"><a href>耐火行业</a></li>
-			<li class="industry3"><a href>钢铁行业</a></li>
-			<li class="industry4"><a href>冶金化工行业</a></li>
-			<li class="industry5"><a href>设备行业</a></li>
-			<li class="industry6"><a href>其他行业</a></li>
-		</ul>
-	</div>
 	<div class="clear"></div>
 </div>
 <div class="main">
--- application/runtime/pc/7c36a14a0831a3836b3679adba41b809.php
+++ application/runtime/pc/7c36a14a0831a3836b3679adba41b809.php
@@ -20,10 +20,11 @@
     <div class="content">
         <div class="logo"></div>
         <ul class="nav">
-            <li><a href='http://info.nainaiwang.com//index/index' class="on">首页</a></li>
-            <li id=""><a href='http://info.nainaiwang.com//hangye/index/type/<?php echo isset($typelist[0]['id'])?$typelist[0]['id']:"";?>'>行业</a></li>
-            <li><a href="http://info.nainaiwang.com//shuju/index/type/<?php echo isset($typelist[1]['id'])?$typelist[1]['id']:"";?>">数据</a></li>
-            <li><a href='http://info.nainaiwang.com//guandian/index/type/<?php echo isset($typelist[2]['id'])?$typelist[2]['id']:"";?>'>观点</a></li>
+            <li><a href='http://info.nainaiwang.com//index/index' <?php if($type_id == 0){?>class='on'<?php }?>>首页</a></li>
+            <li id="industry"><a href='http://info.nainaiwang.com//hangye/index/type/<?php echo isset($typelist[0]['id'])?$typelist[0]['id']:"";?>' <?php if($type_id ==$typelist[0]['id']){?>class="on"<?php }?>>行业</a></li>
+
+            <li><a href="http://info.nainaiwang.com//shuju/index/type/<?php echo isset($typelist[1]['id'])?$typelist[1]['id']:"";?>" <?php if($type_id == $typelist[1]['id']){?>class="on"<?php }?>>数据</a></li>
+            <li><a href='http://info.nainaiwang.com//guandian/index/type/<?php echo isset($typelist[2]['id'])?$typelist[2]['id']:"";?>' <?php if($type_id == $typelist[2]['id']){?>class="on"<?php }?>>观点</a></li>
         </ul>
         <form class="search">
             <input type="text" placeholder="请输入关键字查询" class="text"><input type="button" value="搜索" class="button">
@@ -38,20 +39,22 @@
             });
         })
     </script>
+    
+    <div class="in_nav" id="in_nav">
+        <ul>
+            <img src="/views/pc/images/nav_top.png" class="in_nav_top">
 
+            <?php if(!empty($cates)) foreach($cates as $key => $item){?>
+            <li>
+                <a class="title_a" href="http://info.nainaiwang.com//hangye/index/type/3/id/<?php echo isset($item['id'])?$item['id']:"";?>"><?php echo isset($item['name'])?$item['name']:"";?>
+                    <div class="c-tip-arrow" style="left: 38px;"><em></em></div>
+                </a>
+            </li>
+            <?php }?>
+            <!-- <li class="industry1"><a href>建材行业</a></li> -->
+        </ul>
+    </div>
 
-
-	<div class="in_nav" id="in_nav">
-		<ul>
-			<img src="/views/pc/images/nav_top.png" class="in_nav_top">
-			<li class="industry1"><a href>建材行业</a></li>
-			<li class="industry2"><a href>耐火行业</a></li>
-			<li class="industry3"><a href>钢铁行业</a></li>
-			<li class="industry4"><a href>冶金化工行业</a></li>
-			<li class="industry5"><a href>设备行业</a></li>
-			<li class="industry6"><a href>其他行业</a></li>
-		</ul>
-	</div>
 	<div class="clear"></div>
 </div>
 <div class="main">
@@ -89,30 +92,30 @@
 		<?php if(!empty($type)) foreach($type as $key => $item){?>
 			<div class="data_list">
 				<div class="data_title">
-					<a href><h1><?php echo isset($item['name'])?$item['name']:"";?></h1></a>
-					<a href="http://info.nainaiwang.com//more/index/type/<?php echo isset($item['id'])?$item['id']:"";?>"><span>更多</span></a>
+					<a href="javascript:;"><h1><?php echo isset($item['name'])?$item['name']:"";?></h1></a>
+					<a href="http://info.nainaiwang.com//more/index/type/<?php echo isset($data[$item['id']][0][0]['type'])?$data[$item['id']][0][0]['type']:"";?>"><span>更多</span></a>
 				</div>
 				<div class="data_content">
 					<div class="hot_news">
-						<a href><img src="<?php echo isset($data[$item['id']][0][0]['cover'][0])?$data[$item['id']][0][0]['cover'][0]:"";?>"></a>
+						<a href="http://info.nainaiwang.com//detail/index/id/<?php echo isset($data[$item['id']][0][0]['id'])?$data[$item['id']][0][0]['id']:"";?>"><img src="<?php echo isset($data[$item['id']][0][0]['cover'][0])?$data[$item['id']][0][0]['cover'][0]:"";?>"></a>
 						<div class="hot_point">
-							<a href><?php echo isset($data[$item['id']][0][0]['name'])?$data[$item['id']][0][0]['name']:"";?></a>
+							<a href="http://info.nainaiwang.com//detail/index/id/<?php echo isset($data[$item['id']][0][0]['id'])?$data[$item['id']][0][0]['id']:"";?>"><?php echo isset($data[$item['id']][0][0]['name'])?$data[$item['id']][0][0]['name']:"";?></a>
 						</div>
 					</div>
 					<ul>
 						<?php if(isset($data[$item['id']][0][1]['id'])){?>
 						<li>
-							<a href><img src="<?php echo isset($data[$item['id']][0][1]['cover'][0])?$data[$item['id']][0][1]['cover'][0]:"";?>" title="<?php echo isset($data[$item['id']][0][1]['name'])?$data[$item['id']][0][1]['name']:"";?>" class="list_hot"></a>
+							<a href="http://info.nainaiwang.com//detail/index/id/<?php echo isset($data[$item['id']][0][1]['id'])?$data[$item['id']][0][1]['id']:"";?>"><img src="<?php echo isset($data[$item['id']][0][1]['cover'][0])?$data[$item['id']][0][1]['cover'][0]:"";?>" title="<?php echo isset($data[$item['id']][0][1]['name'])?$data[$item['id']][0][1]['name']:"";?>" class="list_hot"></a>
 							<span class="list_hot_cont">
 								<!-- 此处需要控制字数 -->
-								<p><a href><?php echo isset($data[$item['id']][0][1]['name'])?$data[$item['id']][0][1]['name']:"";?></a></p>
-								<p><a href><?php echo isset($data[$item['id']][0][1]['short_content'])?$data[$item['id']][0][1]['short_content']:"";?></a></p>
+								<p><a href="http://info.nainaiwang.com//detail/index/id/<?php echo isset($data[$item['id']][0][1]['id'])?$data[$item['id']][0][1]['id']:"";?>"><?php echo isset($data[$item['id']][0][1]['name'])?$data[$item['id']][0][1]['name']:"";?></a></p>
+								<p><a href="http://info.nainaiwang.com//detail/index/id/<?php echo isset($data[$item['id']][0][1]['id'])?$data[$item['id']][0][1]['id']:"";?>"><?php echo isset($data[$item['id']][0][1]['short_content'])?$data[$item['id']][0][1]['short_content']:"";?></a></p>
 							</span>
 						</li>
 						<?php }?>
 						<?php if(!empty($data[$item['id']][0])) foreach($data[$item['id']][0] as $key => $item){?>
 							<?php if($key>1&&$key<6){?>
-							<li><a href><?php echo isset($item['name'])?$item['name']:"";?></a></li>
+							<li><a href="http://info.nainaiwang.com//detail/index/id/<?php echo isset($item['id'])?$item['id']:"";?>"><?php echo isset($item['name'])?$item['name']:"";?></a></li>
 							<?php }?>
 						<?php }?>
 					</ul>
@@ -124,8 +127,8 @@
 		<div class="data_box">
 			<ul class="data_list">
 				<li class="title">
-					<a href><h3>数据</h3></a>
-					<a href><span>更多</span></a>
+					<a href="javascript:;"><h3>数据</h3></a>
+					<a href="http://info.nainaiwang.com//more/index/type/4"><span>更多</span></a>
 				</li>
 				<?php if(!empty($main_data[0])) foreach($main_data[0] as $key => $item){?>
 				<li><a href="http://info.nainaiwang.com//detail/index/id/<?php echo isset($item['id'])?$item['id']:"";?>"><?php echo isset($item['name'])?$item['name']:"";?></a></li>
