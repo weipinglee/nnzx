0d8e4e82be4d0fbea116665610ae6bd1e3cab856
weiping <weiping.lee@163.com>
2017-06-21 09:17:29 +0800

    供求信息列表页面

---
+++ application/controllers/Market.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 供求信息列表
+ */
+
+use Library\safe;
+use Library\url;
+class MarketController extends InitController {
+	
+	public function init(){
+		parent::init();
+	}
+
+
+	public function listAction(){
+
+	}
+}
