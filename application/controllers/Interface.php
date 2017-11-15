e79a0c5b70cf2900b6040c4f9cfa8908b71616e4
weiping <weiping.lee@163.com>
2017-11-15 16:03:27 +0800

    交易系统首页资讯接口

---
+++ application/controllers/Interface.php
@@ -0,0 +1,35 @@
+<?php
+use Library\safe;
+use \nainai\Article;
+use \Library\cache\Cache;
+use \Library\json;
+class InterfaceController extends Yaf\Controller_Abstract {
+	
+	public function init(){
+		$this->article = new Article();
+	}
+
+	//交易网站首页资讯数据
+	public function tradewebInfoAction(){
+		$cacheObj = new Cache(array('type'=>'m','expire'=>60));
+		$result = '';
+		if($cacheObj->isActive()){
+			$result = $cacheObj->get('TRADEWEB_INFO');
+		}
+
+		if(!$result){
+			$page = safe::filterGet('page','int',1);
+
+			//主体数据
+			$main_data = $this->article->arcList($page,array(),'','',10,0,0,'INTERFACE');
+			$result = $main_data;
+			if($cacheObj->isActive()){
+				$cacheObj->set('TRADEWEB_INFO',$result);
+			}
+		}
+
+		die(JSON::encode($result));
+
+	}
+
+}
