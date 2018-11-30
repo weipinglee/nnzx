bf71f4118416b0f6e3307b6f65ccd8903f4b2b5e
weipinglee <weiping.lee@163.com>
2018-11-30 17:34:53 +0800

    bootstrap

--- nnys-admin/application/Bootstrap.php
+++ nnys-admin/application/Bootstrap.php
@@ -12,22 +12,11 @@ use \Library\Session\Driver\Db;
 class Bootstrap extends \Yaf\Bootstrap_Abstract{
 
     public function _initConfig(Yaf\Dispatcher $dispatcher) {
-		if(\Library\tool::getConfig('error')){
-			error_reporting(E_ALL);
-		}
-		else{
-			error_reporting(0);
-		}
 		//把配置保存起来
 		$this->config = Yaf\Application::app()->getConfig();
 		Yaf\Registry::set('config', $this->config);
-		define('REQUEST_METHOD', strtoupper($dispatcher->getRequest()->getMethod()));
-		define('IS_GET',        REQUEST_METHOD =='GET' ? true : false);
-		define('IS_POST',       REQUEST_METHOD =='POST' ? true : false);
-		define('IS_PUT',        REQUEST_METHOD =='PUT' ? true : false);
-		define('IS_DELETE',     REQUEST_METHOD =='DELETE' ? true : false);
-		define('IS_AJAX',       ((isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')) ? true : false);
-		
+        $initObj = new \nainai\appInit($dispatcher);
+        $initObj->init();
 		//数据库方式存储session
 		ini_set('session.save_handler','user');
 		$session = new Db('admin_session',7200);
