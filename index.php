8cc5fb69215307a2b1937d0fe9ca2a55afa5b1f2
weipinglee <weiping.lee@163.com>
2018-12-04 11:04:42 +0800

    access origin

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ date_default_timezone_set('Asia/Shanghai');
 define('APPLICATION_PATH', __DIR__);
 
 header("Content-Type:text/html;charset=utf-8");
-header("Access-Control-Allow-Origin:https://www.nainaiwang.com,http://ceshi.nainaiwang.com");
+header("Access-Control-Allow-Origin:https://www.nainaiwang.com");
+header("Access-Control-Allow-Origin:http://ceshi.nainaiwang.com");
 header("Access-Control-Allow-Methods:POST,GET");
 DEFINE('DEVICE_TYPE','pc');
 $application = new Yaf\Application( APPLICATION_PATH . "/conf/application.ini");
